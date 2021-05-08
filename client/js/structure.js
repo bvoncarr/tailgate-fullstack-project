9f4fedc09cb6cf0d19a43beeeabacc301593803d
Beverly <beverlybuscher@gmail.com>
2021-05-08 13:01:12 -0500

    Added swiper, added checkboxes to forms and updated background colors.

---
+++ client/js/structure.js
@@ -0,0 +1,20 @@
+
+
+
+
+var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
